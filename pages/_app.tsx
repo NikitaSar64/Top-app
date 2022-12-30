72e5da53d91953487ea6938ac6c608183248d387
Nikita_dev <nikitasar64@gmail.com>
2022-12-30 18:13:59 +0300

    init: add font, variables in css

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,16 @@
 import { AppProps } from 'next/app';
 import '../styles/globals.css';
+import Head from 'next/head';
 
 export default function App({ Component, pageProps } : AppProps) : JSX.Element {
-  return <Component {...pageProps} />;
+  return (
+        <>
+          <Head>
+            <title>Top app</title>
+            <link rel="preconnect" href="https://fonts.googleapis.com"/>
+            <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@600;700&family=Noto+Sans:wght@300;400;500;700&display=swap" rel="stylesheet"/>
+          </Head>
+          <Component {...pageProps} />
+        </>
+  );
 }
